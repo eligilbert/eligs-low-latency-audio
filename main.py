437083f58bce4df2d39baae8acce05f47cd57ebf
eligilbert <eliasbgilbert@gmail.com>
2020-08-13 00:13:15 -0700

    maybe?

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Server:
             await self.send_to_clients(message)
 
 server = Server()
-start_server = websockets.serve(server.ws_handler, 'localhost', 8080)
+start_server = websockets.serve(server.ws_handler, 'https://eligs-low-latency-audio.herokuapp.com', 8080)
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_server)
 loop.run_forever()
