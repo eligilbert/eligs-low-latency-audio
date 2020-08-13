0a91be71f12e95c3b5f71619138320e18abc8fab
eligilbert <eliasbgilbert@gmail.com>
2020-08-12 23:58:39 -0700

    80

--- ws_consumer.py
+++ ws_consumer.py
@@ -18,12 +18,12 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
 async def consumer_handler(websocket):
     async for message in websocket:
         # print(message)
-        # print("got it!")
+        print("got it!")
         stream.write(message, CHUNK)
 
 async def consume():
-    # websocket_resource_url = "ws://eligs-low-latency-audio.herokuapp.com:8080"
-    websocket_resource_url = "ws://localhost:8080"
+    websocket_resource_url = "ws://eligs-low-latency-audio.herokuapp.com:80"
+    # websocket_resource_url = "ws://localhost:8080"
     async with websockets.connect(websocket_resource_url) as websocket:
         await consumer_handler(websocket)
 
